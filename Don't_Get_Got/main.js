0fa1ce976ff9107bf85b917babf803a3d866e129
wfh111 <wfh661@gmail.com>
2018-01-25 13:56:03 -0800

    Images appear again!!!

--- Don't_Get_Got/main.js
+++ Don't_Get_Got/main.js
@@ -3,88 +3,36 @@ var sheetHeight = 490;
 var myScore;
 
 
-//function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, loop, scale) {
-//    this.spriteSheet = spriteSheet;
-//    this.frameWidth = frameWidth;
-//    this.frameDuration = frameDuration;
-//    this.frameHeight = frameHeight;
-//    this.frames = frames;
-//    this.totalTime = frameDuration * frames;
-//    this.elapsedTime = 0;
-//    this.loop = loop;
-//    this.scale = scale;
-//}
-//
-//Animation.prototype.drawFrame = function (tick, ctx, x, y) {
-//    this.elapsedTime += tick;
-//    if (this.isDone()) {
-//        if (this.loop) this.elapsedTime = 0;
-//    }
-//    var frame = this.currentFrame();
-//    var xindex = 0;
-//    var yindex = 0;
-//    xindex = frame % this.sheetWidth;
-//    yindex = Math.floor(frame / this.sheetWidth);
-//
-//    ctx.drawImage(this.spriteSheet,
-//                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
-//                 this.frameWidth, this.frameHeight,
-//                 x, y,
-//                 this.frameWidth * this.scale,
-//                 this.frameHeight * this.scale);
-//}
-//
-//Animation.prototype.currentFrame = function () {
-//    return Math.floor(this.elapsedTime / this.frameDuration);
-//}
-//
-//Animation.prototype.isDone = function () {
-//    return (this.elapsedTime >= this.totalTime);
-//}
-function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
     this.spriteSheet = spriteSheet;
-    this.startX = startX;
-    this.startY = startY;
     this.frameWidth = frameWidth;
     this.frameDuration = frameDuration;
     this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
     this.frames = frames;
     this.totalTime = frameDuration * frames;
     this.elapsedTime = 0;
     this.loop = loop;
-    this.reverse = reverse;
+    this.scale = scale;
 }
 
-Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
-    var scaleBy = scaleBy || 1;
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
     this.elapsedTime += tick;
-    if (this.loop) {
-        if (this.isDone()) {
-            this.elapsedTime = 0;
-        }
-    } else if (this.isDone()) {
-        return;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
     }
-    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
-    var vindex = 0;
-    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
-        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
-        vindex++;
-    }
-    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
-        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
-        vindex++;
-    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
 
-    var locX = x;
-    var locY = y;
-    var offset = vindex === 0 ? this.startX : 0;
     ctx.drawImage(this.spriteSheet,
-                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-                  this.frameWidth, this.frameHeight,
-                  locX, locY,
-                  this.frameWidth * scaleBy,
-                  this.frameHeight * scaleBy);
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth * this.scale,
+                 this.frameHeight * this.scale);
 }
 
 Animation.prototype.currentFrame = function () {
@@ -94,6 +42,59 @@ Animation.prototype.currentFrame = function () {
 Animation.prototype.isDone = function () {
     return (this.elapsedTime >= this.totalTime);
 }
+//function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+//    this.spriteSheet = spriteSheet;
+//    this.startX = startX;
+//    this.startY = startY;
+//    this.frameWidth = frameWidth;
+//    this.frameDuration = frameDuration;
+//    this.frameHeight = frameHeight;
+//    this.frames = frames;
+//    this.totalTime = frameDuration * frames;
+//    this.elapsedTime = 0;
+//    this.loop = loop;
+//    this.reverse = reverse;
+//}
+//
+//Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+//    var scaleBy = scaleBy || 1;
+//    this.elapsedTime += tick;
+//    if (this.loop) {
+//        if (this.isDone()) {
+//            this.elapsedTime = 0;
+//        }
+//    } else if (this.isDone()) {
+//        return;
+//    }
+//    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
+//    var vindex = 0;
+//    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
+//        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
+//        vindex++;
+//    }
+//    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
+//        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
+//        vindex++;
+//    }
+//
+//    var locX = x;
+//    var locY = y;
+//    var offset = vindex === 0 ? this.startX : 0;
+//    ctx.drawImage(this.spriteSheet,
+//                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+//                  this.frameWidth, this.frameHeight,
+//                  locX, locY,
+//                  this.frameWidth * scaleBy,
+//                  this.frameHeight * scaleBy);
+//}
+//
+//Animation.prototype.currentFrame = function () {
+//    return Math.floor(this.elapsedTime / this.frameDuration);
+//}
+//
+//Animation.prototype.isDone = function () {
+//    return (this.elapsedTime >= this.totalTime);
+//}
 //Score to display on canvas
 /*function scoreChange(width, height, color, x, y, type) {
 	this.type = type;
@@ -147,7 +148,7 @@ Background.prototype.update = function () {
 };
 
 function MushroomDude(game, spritesheet) {
-    this.animation = new Animation(spritesheet, 0, 0, 189, 230, 5, 0.10, 14, true);
+    this.animation = new Animation(spritesheet, 189, 230, 5, 0.10, 14, true,1);
     this.x = 0;
     this.y = 0;
     this.speed = 200;
@@ -159,7 +160,7 @@ function MushroomDude(game, spritesheet) {
 }
 
 MushroomDude.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x + 150, this.y + 100, 1);
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x + 150, this.y + 100);
 }
 
 MushroomDude.prototype.update = function () {
@@ -196,8 +197,9 @@ MushroomDude.prototype.update = function () {
     }
 }
 
+//0,512
 function Spike (game, spritesheet, lane) {
-	this.animation = new Animation(spritesheet, 0, 512, 142, 163, 810, 1, 1, true);
+	this.animation = new Animation(spritesheet, 142, 163, 810, 1, 1, true,1);
 	this.speed = 60;
 	this.ctx = game.ctx;
 	if (lane === 0) {
@@ -218,13 +220,13 @@ Spike.prototype.update = function() {
 };
 
 Spike.prototype.draw = function () {
-	this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.4);
+	this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);//0.4
     Entity.prototype.draw.call(this);
 };
-
+//0,0
 // inheritance
 function Crate(game, spritesheet, lane) {
-    this.animation = new Animation(spritesheet, 0, 0, 512, 512, 810, 1, 1, true);
+    this.animation = new Animation(spritesheet, 512, 512, 810, 1, 1, true,1);
     this.speed = 60;
     this.ctx = game.ctx;
     if (lane === 0) {
@@ -245,12 +247,12 @@ Crate.prototype.update = function () {
 };
 
 Crate.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.1);
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);//.1
     Entity.prototype.draw.call(this);
 };
-
+//0, 1300
 function Oil(game, spritesheet, lane) {
-    this.animation = new Animation(spritesheet, 0, 1300, 776, 484, 810, 1, 1, true);
+    this.animation = new Animation(spritesheet, 776, 484, 810, 1, 1, true,1);
     this.speed = 60;
     this.ctx = game.ctx;
     if (lane === 0) {
@@ -271,12 +273,12 @@ Oil.prototype.update = function () {
 };
 
 Oil.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.2);
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);//.2
     Entity.prototype.draw.call(this);
 };
-
+//0, 675
 function Branch(game, spritesheet, lane) {
-    this.animation = new Animation(spritesheet, 0, 675, 800, 600, 810, 1, 1, true);
+    this.animation = new Animation(spritesheet, 800, 600, 810, 1, 1, true,1);
     this.speed = 60;
     this.ctx = game.ctx;
     if (lane === 0) {
@@ -298,7 +300,7 @@ Branch.prototype.update = function () {
 };
 
 Branch.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.1);
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y); //0.1
     Entity.prototype.draw.call(this);
 };
 
@@ -326,26 +328,26 @@ AM.downloadAll(function () {
     gameEngine.start();
     gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/bg3.png")));
     gameEngine.addEntity(new MushroomDude(gameEngine, AM.getAsset("./img/mushroomdude.png")));
-    var type = Math.floor(Math.random() * 100) + 1;
-    type %= 4;
-//    var type = 0;
-    var lane = Math.floor(Math.random() * 10) + 1;
-    lane %= 3;
-//    var lane = 2;
-    switch(type) {
-    case 0: //Spikes
-    	gameEngine.addEntity(new Spike(gameEngine, AM.getAsset("./img/Spikes.png"), lane));
-    	break;
-    case 1: //Crate
-        gameEngine.addEntity(new Crate(gameEngine, AM.getAsset("./img/Crate.png"), lane));
-        break;
-    case 2: //Oil
-    	gameEngine.addEntity(new Oil(gameEngine, AM.getAsset("./img/newOil.png"), lane));
-    	break;
-    case 3: //Branch
-    	gameEngine.addEntity(new Branch(gameEngine, AM.getAsset("./img/branch.png"), lane));
-    	break;
-    }
+//    var type = Math.floor(Math.random() * 100) + 1;
+//    type %= 4;
+////    var type = 0;
+//    var lane = Math.floor(Math.random() * 10) + 1;
+//    lane %= 3;
+////    var lane = 2;
+//    switch(type) {
+//    case 0: //Spikes
+    	gameEngine.addEntity(new Spike(gameEngine, AM.getAsset("./img/Spikes.png"), 1));
+//    	break;
+//    case 1: //Crate
+//        gameEngine.addEntity(new Crate(gameEngine, AM.getAsset("./img/Crate.png"), lane));
+//        break;
+//    case 2: //Oil
+//    	gameEngine.addEntity(new Oil(gameEngine, AM.getAsset("./img/newOil.png"), lane));
+//    	break;
+//    case 3: //Branch
+//    	gameEngine.addEntity(new Branch(gameEngine, AM.getAsset("./img/branch.png"), lane));
+//    	break;
+//    }
 //    gameEngine.addEntity(new Obstacle_Spawner(gameEngine, AM.getAsset("./img/obstacles.png")))
     console.log("All Done!");
 });
